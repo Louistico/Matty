4628564da7b6950f1435509fd3ee942dc0eb3f88
louisosio <louisosiomu@gmail.com>
2022-05-06 09:33:18 +0200

    Eliminar boton Cancelar

--- app/src/main/java/com/example/matty/Payment.kt
+++ app/src/main/java/com/example/matty/Payment.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.example.matty.ui.sale.SaleFragment
 
 class Payment : AppCompatActivity() {
 
@@ -17,8 +16,10 @@ class Payment : AppCompatActivity() {
     }
 
     fun Return(view:View){
-       val intentRet=Intent(this,Menu::class.java)
-        startActivity(intentRet)
+        onBackPressed()
+    }
+    fun Atras(view: View){
+        onBackPressed()
     }
 
     fun onClickMethods(view: View){
@@ -26,5 +27,4 @@ class Payment : AppCompatActivity() {
         startActivity(intentconfirm)
 
     }
-
 }
